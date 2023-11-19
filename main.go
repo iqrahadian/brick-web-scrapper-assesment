09680075c293ffc14956fd59f090889b5a0c6004
Iqbal Rahadian <iqbalrahadian29@gmail.com>
2023-11-19 23:07:54 +0700

    remove httpclient

--- main.go
+++ main.go
@@ -11,31 +11,7 @@ import (
 	"github.com/iqrahadian/brick-web-scrapper-assesment/scrapper"
 )
 
-func mainSingleProduct() {
-
-	// init http client with rate limiter
-	// rateLimit := rate.NewLimiter(rate.Every(10*time.Second), 50) // 50 request every 10 seconds
-	// httpClient := httpclient.NewClient(rateLimit)
-
-	// tokpedUrl := "https://www.tokopedia.com/duniagadgetku/xiaomi-redmi-10-4-64-gb-6-128-gb-garansi-resmi-6-128-promo?extParam=cmp%3D1%26ivf%3Dtrue"
-
-	// stringHtml, err := scrapper.RetrieveProductDetailPage(tokpedUrl)
-	// if err != nil {
-	// 	fmt.Println(fmt.Errorf("Failed to retrieve product page", err))
-	// }
-
-	// fmt.Println("HTML : ", stringHtml)
-
-	// product := model.Product{ProductUrl: tokpedUrl}
-	// product, err := scrapper.ScrapProductDetailPage(&httpClient, product)
-	// if err != nil {
-	// 	fmt.Println(fmt.Errorf("Failed to retrieve product page", err))
-	// }
-
-}
-
-// func mainProductList() {
-func main() {
+func main2() {
 
 	var wg sync.WaitGroup
 
@@ -72,16 +48,15 @@ func main() {
 		fmt.Println("===========================")
 	}
 
-	// fmt.Println(produ)
-
 	wg.Wait()
 
 }
 
-func main2() {
+func main() {
 
 	//init http client with rate limiter
-	headlessClient := headlessclient.NewClient(10, 10*time.Second)
+	headlessClient := headlessclient.NewClient(10, 5*time.Second) // to scrap product detail
+	// headlessClient2 := headlessclient.NewClient(10, 10*time.Second) // to scrap product list
 
 	var (
 		wg         sync.WaitGroup
@@ -92,51 +67,71 @@ func main2() {
 
 	// init database
 
-	tokpedUrl := []string{
-		"https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=1",
-		"https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=2",
-	}
+	// tokpedUrl := []string{
+	// 	"https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=1",
+	// 	"https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=2",
+	// }
 
-	productList := []model.Product{}
+	// productList := []model.Product{}
 
-	productLen := 0
-	for _, url := range tokpedUrl {
-		if productLen > 100 {
+	// productLen := 0
+	// for _, url := range tokpedUrl {
+	// 	if productLen > 10 {
+	// 		break
+	// 	}
+
+	// 	products, err := scrapper.ScrapProductListPage(&headlessClient2, url)
+	// 	if err != nil {
+	// 		fmt.Errorf(
+	// 			fmt.Sprintf("Failed to retrieve Product list page : ", url),
+	// 			err,
+	// 		)
+	// 	}
+
+	// 	productList = append(productList, products...)
+	// 	productLen += 1
+	// }
+	productList, _ := scrapper.ExtractProductList(page1)
+
+	for idx, product := range productList {
+		// fmt.Println(product.Name, product.Price)
+		if idx > 5 {
 			break
 		}
-
-		products, err := scrapper.ScrapProductListPage(&headlessClient, url)
-		if err != nil {
-			fmt.Errorf(
-				fmt.Sprintf("Failed to retrieve Product list page : ", url),
-				err,
-			)
-		}
-
-		productList = append(productList, products...)
-		productLen += 1
+		jobChannel <- product
 	}
 
-	for _, product := range productList {
-		fmt.Println(product.Name, product.Price)
-		// jobChannel <- product
-	}
+	close(jobChannel)
+	wg.Wait()
 
-	fmt.Println("PRODUCT LEN :", len(productList))
+	fmt.Println("GOT HERE DONE")
 
 }
 
 func startProductScrapper(wg *sync.WaitGroup, client *headlessclient.RLHeadlessClient, productChannel <-chan model.Product) {
-	for w := 1; w <= 5; w++ {
+	for worker := 1; worker <= 5; worker++ {
 		wg.Add(1)
-		go func(wg *sync.WaitGroup, productChannel <-chan model.Product) {
+		go func(w *sync.WaitGroup, pc <-chan model.Product) {
 
 			defer wg.Done()
-			for {
-				select {
-				case product := <-productChannel:
-					scrapper.ScrapProductDetailPage(client, product)
+
+			for product := range pc {
+				if !strings.Contains(product.ProductUrl, "https://ta.tokopedia.com") {
+					product, err := scrapper.ScrapProductDetailPage(client, product)
+					if err != nil {
+						fmt.Println("Name : ", product.Name)
+						fmt.Println("ERROR : ", err)
+					} else {
+						fmt.Println("Name : ", product.Name)
+						fmt.Println("Price : ", product.Price)
+						fmt.Println("Rate : ", product.Rating)
+					}
+				} else {
+					fmt.Println("Name : ", product.Name)
+					fmt.Println("Skipping because ta.tokopedia")
 				}
+
+				fmt.Println("================================")
 			}
 
 		}(wg, productChannel)
